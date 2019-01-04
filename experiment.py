Moved softmax to outside model, added alt losses, added cuda to loss, and fixed bug where n_samples was being ignored

experiment.py
@@ -8,6 +8,7 @@ import os
 import torch
 
 import data
+import losses
 import model
 import train
 from vis import generate_between_classes
@@ -50,7 +51,8 @@ def run(dataset='mnist', n_samples=50000, n_bins=4,
     # Define loss fcn, incl. label formatting from input
     def input2label(x):
         return torch.squeeze(torch.round((n_bins-1)*x).type(torch.LongTensor),1)
-    loss_fcn = torch.nn.NLLLoss()
+    loss_fcns = {'standard': losses.standard_loss_function}
+    loss_fcn = loss_fcns[loss]
 
     # Train
     train.fit(train_loader, val_loader, n_samples, net, exp_dir, input2label, loss_fcn,
@@ -72,10 +74,10 @@ debug = False
 if debug:
     run(dataset='mnist', n_samples=10, n_bins=4,
         n_features=200, batch_size=64, n_layers=6,
-        loss='standard', optimizer='adam', learnrate=1e-4, dropout=0.9, max_epochs=35, cuda=True, resume=False,
+        loss='standard', optimizer='adam', learnrate=1e-4, dropout=0.9, max_epochs=100, cuda=True, resume=False,
         exp_dir='out', note='')
 else:
     run(dataset='mnist', n_samples=50000, n_bins=4,
         n_features=200, batch_size=64, n_layers=6,
-        loss='standard', optimizer='adam', learnrate=1e-4, dropout=0.9, max_epochs=35, cuda=True, resume=False,
+        loss='standard', optimizer='adam', learnrate=1e-4, dropout=0.9, max_epochs=100, cuda=True, resume=False,
         exp_dir='out', note='')

model.py
@@ -95,7 +95,7 @@ class PixelCNN(nn.Module):
         # Last layer: project to n_bins (output is [-1, n_bins, h, w])
         self.dropout = nn.Dropout2d(dropout)
         self.layers.append(GatedRes(n_features,n_bins,n_classes))
-        self.layers.append(nn.LogSoftmax(dim=1))
+        # self.layers.append(nn.LogSoftmax(dim=1)) # moved out of model
 
     def forward(self,x,y):
         # Add channel of ones so network can tell where padding is
@@ -121,8 +121,8 @@ class PixelCNN(nn.Module):
         x = self.dropout(x)
         i += 1
         x = self.layers[i](x,y)
-        i += 1
-        x = self.layers[i](x)
+        # i += 1 # moved out of model
+        # x = self.layers[i](x) # moved out of model
         assert i==len(self.layers)-1
         assert len(features)==0
         return x

train.py
@@ -16,8 +16,8 @@ from skimage.transform import resize
 import torch
 from torch.autograd import Variable
 
+import losses
 from vis import plot_stats, clearline, generate, tile_images
-
 
 def generate_images(model,img_size,n_classes,onehot_fcn,cuda=True):
     y = np.array(list(range(min(n_classes,10)))*5)  # gpu mem limit
@@ -50,6 +50,7 @@ def fit(train_loader, val_loader, n_samples, model, exp_path, label_preprocess, 
 
     if cuda:
         model = model.cuda()
+        loss_fcn = loss_fcn.cuda()
 
     if not os.path.isdir(exp_path):
         os.makedirs(exp_path)
@@ -88,7 +89,7 @@ def fit(train_loader, val_loader, n_samples, model, exp_path, label_preprocess, 
 
     def epoch(dataloader,training):
         bar = ProgressBar()
-        losses = []
+        epoch_losses = []
         mean_outs = []
         sample_idx = 0
         for x,y in bar(dataloader):
@@ -111,12 +112,12 @@ def fit(train_loader, val_loader, n_samples, model, exp_path, label_preprocess, 
             if training:
                 loss.backward()
                 optimizer.step()
-            losses.append(loss.data.cpu().numpy())
-            sample_idx += 1
-            if sample_idx == n_samples:
+            epoch_losses.append(loss.data.cpu().numpy())
+            sample_idx += x.shape[0]
+            if sample_idx >= n_samples:
                 break
         clearline()
-        return float(np.mean(losses)), np.mean(mean_outs)
+        return float(np.mean(epoch_losses)), np.mean(mean_outs)
 
     for e in range(start_epoch,max_epochs):
         # Training

vis.py
@@ -14,6 +14,7 @@ import numpy as np
 from progressbar import ProgressBar
 import torch
 from torch.autograd import Variable
+import torch.nn.functional as F
 
 
 def plot_stats(stats,savepath):
@@ -69,7 +70,7 @@ def generate(model, img_size, y, temp=0.8, cuda=True):
     for r in bar(range(img_size[0])):
         for c in range(img_size[1]):
             out = model(gen, y)
-            p = torch.exp(out)[:, :, r, c]
+            p = F.softmax(out[:,:,r,c], dim=1)
             p = torch.pow(p, 1/temp)
             p = p/torch.sum(p, -1, keepdim=True)
             sample = p.multinomial(1)